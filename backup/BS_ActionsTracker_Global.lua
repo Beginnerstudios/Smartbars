521cdd9bce972b055827e8aa43a6a9f016f75e22
metixgosu <martin.masika@icloud.com>
2021-12-03 15:08:46 +0100

    0004

--- backup/BS_ActionsTracker_Global.lua
+++
@@ -1,8 +0,0 @@
---NameSpaces------------------------------
-local _,BS_ActionsTracker = ...;
-BS_ActionsTracker.Global ={};
---LoadingMessage--------------------------
---Global variables
-IsActionsTrackerPrimaryFrameVisible = false
-IsActionsTrackerConfigMode = false
--- Revision version Build 0004 --
