eaac99e83677f375784bc1d694865d9a504d61d4
metixgosu <martin.masika@icloud.com>
2022-01-21 12:43:58 +0100

    v - 1.1.1

--- SmartBars_Global.lua
+++ SmartBars_Global.lua
@@ -6,8 +6,8 @@ local Global=SmartBars.Global
 ---Init-------------------------------------
 -- Global functions-----------------------
 function Global:GetSmartBarsInfo()
-  --  local smartBarsPublicVersion,smartBarsPublicBuild = SmartBars.Config:GetSmartBarsInfo() 
-  --  return SmartBars.Localization.AddonName(),smartBarsPublicVersion,smartBarsPublicBuild 
+    local smartBarsPublicVersion,smartBarsPublicBuild = SmartBars.Config:GetSmartBarsInfo() 
+    return SmartBars.Localization.AddonName(),smartBarsPublicVersion,smartBarsPublicBuild 
 end
 -- Revision version v1.1.0 ----------------
 
